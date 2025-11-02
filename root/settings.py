067e2ef88bac8754d9f27e6c5d1f3b226399243b
jmavlonov <jasurmavloniy24@gmail.com>
2025-11-03 00:18:34 +0500

    is_local updated 3

--- root/settings.py
+++ root/settings.py
@@ -110,15 +110,15 @@ if IS_LOCAL:
 else:
     # ➤ Productionda PostgreSQL ishlaydi
     DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',  
-        'NAME': os.getenv('DB_NAME'),  
-        'USER': os.getenv('DB_USER'),  
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),  
-        'PORT': os.getenv('DB_PORT'), 
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',  
+            'NAME': os.getenv('DB_NAME'),  
+            'USER': os.getenv('DB_USER'),  
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),  
+            'PORT': os.getenv('DB_PORT'), 
+        }
     }
-}
 
 # done
 
