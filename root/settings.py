b8a40513cdd838b1f1b6ff9b9b907313525baa3f
jmavlonov <jasurmavloniy24@gmail.com>
2025-11-03 01:27:39 +0500

    1234

--- root/settings.py
+++ root/settings.py
@@ -96,7 +96,20 @@ WSGI_APPLICATION = 'root.wsgi.application'
 #     'default': dj_database_url.config(default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
 # }
 
-DATABASES = {
+
+IS_LOCAL = os.getenv('IS_LOCAL', 'False').strip().lower() in ('true', '1', 'yes')
+
+if IS_LOCAL:
+    print("🧱 Using SQLite (Local Mode)")
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    print("🐘 Using PostgreSQL (Production Mode)")
+    DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': os.getenv('DB_NAME'),
@@ -105,9 +118,7 @@ DATABASES = {
             'HOST': os.getenv('DB_HOST'),
             'PORT': os.getenv('DB_PORT', '5432'),
         }
-}
-
-# done
+    }
 
 
 
