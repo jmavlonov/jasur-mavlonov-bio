3609d94e1796b63b7cec9dce9ea7736c40516956
jmavlonov <jasurmavloniy24@gmail.com>
2025-11-04 22:37:33 +0500

    urls updated

--- root/urls.py
+++ root/urls.py
@@ -36,12 +36,8 @@ urlpatterns += i18n_patterns(
 
 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-  
-else:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    
-    
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 handler404 = 'blog.views.custom_404'
 
